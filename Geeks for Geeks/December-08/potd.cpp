c60cc947b55b3ea02893da7336dcd434fa62c98f
Ankur Nagila <73011858+ANkurNagila@users.noreply.github.com>
2022-12-09 00:00:15 +0530

    Update potd.cpp

--- Geeks for Geeks/December-08/potd.cpp
+++ Geeks for Geeks/December-08/potd.cpp
@@ -1,1 +1,36 @@
+class Solution
+{
+public:
+    bool is_prime(int N){
+        int i=2;
+        for(;i<=sqrt(N);i++){
+            if(N%i==0)
+                return false;
+        }
+        
+        return true;
+    } 
 
+    vector<int> threeDivisors(vector<long long> query, int q)
+    {
+        vector<int> ans;
+        
+        for(auto it:query){
+            int val=sqrt(it);
+            
+            int count=0;
+            
+            for(int i=2;i<=val;i++){
+                if(is_prime(i) && i*i<=it){
+                        count++;
+                }
+                else
+                    continue;
+            }
+            
+            ans.push_back(count);
+        }
+        
+        return ans;
+    }
+};
